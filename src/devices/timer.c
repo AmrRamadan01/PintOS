8c9e348c1e6292cabaa73d57d0b17fc62288d348
Amr Ramadan <78983720+AmrRamadan01@users.noreply.github.com>
2023-04-29 03:41:08 +0300

    initial upload

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -92,8 +92,11 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+
+  /* origianl code commmented out */
+  // while (timer_elapsed (start) < ticks) 
+  //   thread_yield ();
+
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
