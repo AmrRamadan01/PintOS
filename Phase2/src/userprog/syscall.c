4ca1521a6ddd0030166451996278269062a448d8
aliemadabdo <a.e.a3426@gmail.com>
2023-05-22 11:43:25 +0300

    initial commit
    The version before using using Waqee code, has only some comments

---
+++ Phase2/src/userprog/syscall.c
@@ -0,0 +1,20 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+static void syscall_handler (struct intr_frame *); //not in syscall.h because it's static (encapsulation)
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
+{
+  printf ("system call!\n"); //how does we print?
+  thread_exit ();
+}
